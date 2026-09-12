8a6e6189bef59329ecf4e8ae0f63e5fdf0d68fee
xoztass <134248503+xoztass@users.noreply.github.com>
2026-09-12 16:06:09 +0000

    Ilk yemek tarifleri

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Yemek_Tarifleri_Sitem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
